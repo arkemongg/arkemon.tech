f754cd6b574702ecddf1c1670248dffaae6a8eb1
arkemongg <ark.emongg@gmail.com>
2023-07-10 20:04:33 +0600

    preload images

--- js/main.js
+++ js/main.js
@@ -4,14 +4,39 @@ body.style.backgroundSize = "500%"
 body.style.backgroundPosition = ''
 body.style.transition = '0.5s'
 
-let i = 1;
-setInterval(() => {
-    body.style.background = `url('/images/${i}.jpg')`
-    i++;
-    body.style.backgroundSize = "500%"
-    body.style.backgroundPosition = ''
-    body.style.transition = '0.5s'
-    if(i==38){
-        i = 0;
-    }
-}, 5000);
+const images = [];
+let currentIndex = 0;
+
+// Preload the images
+for (let i = 0; i < 39; i++) {
+  const image = new Image();
+  image.src = `/images/${i}.jpg`;
+  images.push(image);
+}
+
+
+function changeBackground() {
+  body.style.background = `url('${images[currentIndex].src}')`;
+  body.style.backgroundSize = '500%';
+  body.style.backgroundPosition = '';
+  body.style.transition = '0.5s';
+
+  currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Call changeBackground once to display the first image instantly
+changeBackground();
+
+// Preload the remaining images before starting the interval
+let preloadingIndex = 1;
+const preloadingInterval = setInterval(() => {
+  const image = new Image();
+  image.src = `/images/${preloadingIndex}.jpg`;
+  images.push(image);
+
+  preloadingIndex++;
+  if (preloadingIndex === 39) {
+    clearInterval(preloadingInterval);
+    setInterval(changeBackground, 5000);
+  }
+}, 0);
