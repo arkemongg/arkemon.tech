b7c4df6cbaf30e22bddb49228867b3d28c7cb4da
arkemongg <ark.emongg@gmail.com>
2023-07-10 19:52:30 +0600

    added js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const body = document.querySelector('body')
+body.style.background = `url('/images/${0}.jpg')`
+body.style.backgroundSize = "500%"
+body.style.backgroundPosition = ''
+body.style.transition = '0.5s'
+
+let i = 1;
+setInterval(() => {
+    body.style.background = `url('/images/${i}.jpg')`
+    i++;
+    body.style.backgroundSize = "500%"
+    body.style.backgroundPosition = ''
+    body.style.transition = '0.5s'
+    if(i==39){
+        i = 0;
+    }
+}, 5000);
