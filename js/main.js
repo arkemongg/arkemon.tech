0de6d7afa26c019f71abe90f0a84c63b5b49f8d3
arkemongg <ark.emongg@gmail.com>
2023-08-11 02:49:47 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,44 +1,11 @@
-const body = document.querySelector('body')
-body.style.background = `url('/images/${0}.jpg')`
-body.style.backgroundSize = "500%"
-body.style.backgroundPosition = ''
-body.style.transition = '0.5s'
+var audio = new Audio("/assets/ncm.mp3")
 
-const images = [];
-let currentIndex = 0;
+var body = document.querySelector("body");
 
+body.addEventListener("mouseenter", function() {
+  audio.play()
+})
+body.addEventListener("touchstart", function() {
+  audio.play()
+})
 
-for (let i = 0; i < 39; i++) {
-  const image = new Image();
-  image.src = `/images/${i}.jpg`;
-  images.push(image);
-}
-
-function changeBackground() {
-  body.style.background = `url('${images[currentIndex].src}')`;
-  body.style.backgroundSize = '500%';
-  body.style.backgroundPosition = '';
-  body.style.transition = '0.5s';
-
-  currentIndex = (currentIndex + 1) % images.length;
-}
-
-
-changeBackground();
-
-
-let preloadingIndex = 1;
-const preloadingInterval = setInterval(() => {
-  const image = new Image();
-  image.src = `/images/${preloadingIndex}.jpg`;
-
-  image.onload = () => {
-    images.push(image);
-
-    preloadingIndex++;
-    if (preloadingIndex === 39) {
-      clearInterval(preloadingInterval);
-      setInterval(changeBackground, 5000);
-    }
-  };
-}, 0);
